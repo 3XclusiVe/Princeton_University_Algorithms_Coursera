fb1ae88ef8d21f9768974e8ecf7f059bfd74d82f
Dmitriy Malych <malllkin007@gmail.com>
2017-06-19 17:02:11 +0300

    Почти завершил класс Point

--- src/Pattern_Recognition/Point.java
+++ src/Pattern_Recognition/Point.java
@@ -10,10 +10,15 @@ package Pattern_Recognition;
  *
  ******************************************************************************/
 
-import java.util.Comparator;
 import edu.princeton.cs.algs4.StdDraw;
 
+import java.util.Comparator;
+
+import static edu.princeton.cs.algs4.StdOut.println;
+
 public class Point implements Comparable<Point> {
+
+    private final Comparator<Point> SLOPE_ORDER = new SlopeOrder();
 
     private final int x;     // x-coordinate of this point
     private final int y;     // y-coordinate of this point
@@ -21,11 +26,10 @@ public class Point implements Comparable<Point> {
     /**
      * Initializes a new point.
      *
-     * @param  x the <em>x</em>-coordinate of the point
-     * @param  y the <em>y</em>-coordinate of the point
+     * @param x the <em>x</em>-coordinate of the point
+     * @param y the <em>y</em>-coordinate of the point
      */
     public Point(int x, int y) {
-        /* DO NOT MODIFY */
         this.x = x;
         this.y = y;
     }
@@ -55,11 +59,22 @@ public class Point implements Comparable<Point> {
      * Double.POSITIVE_INFINITY if the line segment is vertical;
      * and Double.NEGATIVE_INFINITY if (x0, y0) and (x1, y1) are equal.
      *
-     * @param  that the other point
+     * @param that the other point
      * @return the slope between this point and the specified point
      */
     public double slopeTo(Point that) {
-        /* YOUR CODE HERE */
+        double deltaY = that.y - this.y;
+        double deltaX = that.x - this.x;
+
+        if ((deltaX == 0) && (deltaY == 0)) {
+            return Double.NEGATIVE_INFINITY;
+        } else if (deltaX == 0) {
+            return Double.POSITIVE_INFINITY;
+        } else if (deltaY == 0) {
+            return 0;
+        } else {
+            return deltaY / deltaX;
+        }
     }
 
     /**
@@ -67,27 +82,25 @@ public class Point implements Comparable<Point> {
      * Formally, the invoking point (x0, y0) is less than the argument point
      * (x1, y1) if and only if either y0 < y1 or if y0 = y1 and x0 < x1.
      *
-     * @param  that the other point
+     * @param that the other point
      * @return the value <tt>0</tt> if this point is equal to the argument
-     *         point (x0 = x1 and y0 = y1);
-     *         a negative integer if this point is less than the argument
-     *         point; and a positive integer if this point is greater than the
-     *         argument point
+     * point (x0 = x1 and y0 = y1);
+     * a negative integer if this point is less than the argument
+     * point; and a positive integer if this point is greater than the
+     * argument point
      */
     public int compareTo(Point that) {
 
-        int eqaual = 0;
-
-        int yCoordCompare = (int) Math.signum(this.y - that.y);
-        if(yCoordCompare == eqaual) {
-            int xCoordCompare = (int) Math.signum(this.x - that.x);
-            if(xCoordCompare == eqaual) {
-                return eqaual;
-            } else {
-                return xCoordCompare;
-            }
+        if (this.y > that.y) {
+            return 1;
+        } else if (this.y < that.y) {
+            return -1;
+        } else if (this.x > that.x) {
+            return 1;
+        } else if (this.x < that.x) {
+            return -1;
         } else {
-            return yCoordCompare;
+            return 0;
         }
     }
 
@@ -98,7 +111,7 @@ public class Point implements Comparable<Point> {
      * @return the Comparator that defines this ordering on points
      */
     public Comparator<Point> slopeOrder() {
-        /* YOUR CODE HERE */
+        return null;
     }
 
 
@@ -110,14 +123,82 @@ public class Point implements Comparable<Point> {
      * @return a string representation of this point
      */
     public String toString() {
-        /* DO NOT MODIFY */
         return "(" + x + ", " + y + ")";
     }
+
+
+    private class SlopeOrder implements Comparator<Point> {
+
+        @Override
+        public int compare(Point o1, Point o2) {
+            double o1Slope = slopeTo(o1);
+            double o2Slope = slopeTo(o2);
+
+            if (o1Slope > o2Slope) {
+                return 1;
+            } else if (o1Slope < o2Slope) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+    }
+
 
     /**
      * Unit tests the Point data type.
      */
     public static void main(String[] args) {
-        /* YOUR CODE HERE */
+
+        checkCompareTo();
+
+        checkSlopeTo();
+
     }
+
+    private static void checkSlopeTo() {
+        Point p1 = new Point(1, 1);
+        Point p2 = new Point(1, 0);
+        Point p3 = new Point(0, 1);
+        Point p0 = new Point(0, 0);
+
+        testCase(p1.slopeTo(p1) == Double.NEGATIVE_INFINITY);
+        testCase(p1.slopeTo(p2) == Double.POSITIVE_INFINITY);
+        testCase(p3.slopeTo(p1) == 0);
+        testCase(p0.slopeTo(p1) == 1);
+    }
+
+    private static void checkCompareTo() {
+        Point p1 = new Point(1, 1);
+        Point p2 = new Point(1, 0);
+        Point p3 = new Point(0, 1);
+        Point p0 = new Point(0, 0);
+
+
+
+        testCase(p1.compareTo(p1) == 0);
+        testCase(p2.compareTo(p2) == 0);
+        testCase(p3.compareTo(p3) == 0);
+
+        testCase(p1.compareTo(p2) == 1);
+        testCase(p1.compareTo(p3) == 1);
+
+        testCase(p2.compareTo(p3) == -1);
+    }
+
+    private static void testCase(boolean testCase) {
+        if (testCase == true) {
+            println("PASSED");
+        } else {
+            println("FAILED");
+            try {
+                throw new Exception();
+            } catch (Exception e) {
+                //for fast navigate to error in code
+                e.printStackTrace();
+            }
+
+        }
+    }
+
 }
